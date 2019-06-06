25c6cac139576fe2861f2aadfc917285e48f01c4
AnaPaulaRolim <ana.paularolim@hotmail.com>
2019-06-06 20:21:02 -0300

    trabalhoG2

---
+++ script.js
@@ -0,0 +1,10 @@
+$("section").load('home.html');
+function home(){
+    $("section").load('home.html');
+}
+function loja(){
+    $("section").load('loja.html');
+}
+function Contato(){
+    $("section").load('Contato.html');
+}
